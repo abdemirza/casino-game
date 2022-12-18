85e2a7ed62665b3887023ced50b165bba817615c
Abuzar Mirza <abuzar.mirza@easyeat.ai>
2022-12-18 16:21:44 +0530

    added dollar icon and headings

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -2,7 +2,9 @@ import {View, StyleSheet, Image, Text} from 'react-native';
 import React from 'react';
 
 import {primary, white} from '../constants/colors';
-import CustomButton from '../components/CustomButton';
+import CustomButton from '@components/CustomButton';
+
+import ic_dollar from '@images/ic_dollar.png';
 
 const MainScreen = () => {
   const onPressHandler = () => {
@@ -10,7 +12,11 @@ const MainScreen = () => {
   };
   return (
     <View style={styles.container}>
-      <View style={styles.blueContainer}></View>
+      <View style={styles.blueContainer}>
+        <Image source={ic_dollar} style={styles.dollarIcon} />
+        <Text style={styles.heading}>TOTAL BALANCE</Text>
+        <Text style={styles.balanceText}>24 358.50</Text>
+      </View>
       <View style={styles.whiteContainer}>
         <View />
         <CustomButton onPress={onPressHandler} title="Start the game" />
@@ -27,7 +33,6 @@ const styles = StyleSheet.create({
     height: '40%',
     backgroundColor: primary,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   whiteContainer: {
     flex: 1,
@@ -35,6 +40,21 @@ const styles = StyleSheet.create({
     height: '60%',
     backgroundColor: white,
     justifyContent: 'space-between',
+  },
+  heading: {
+    fontSize: 14,
+    fontWeight: '700',
+    color: 'white',
+    marginBottom: 3,
+  },
+  balanceText: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: 'white',
+  },
+  dollarIcon: {
+    marginBottom: 10,
+    marginTop: 36,
   },
 });
 export default MainScreen;
